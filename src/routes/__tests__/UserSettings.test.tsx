20a8459fae240f1a17ea11d58479a01c56470111
Artem Bosenko <arbosenk@cisco.com>
2023-05-18 01:26:10 +0300

    add for unit testing

--- src/routes/__tests__/UserSettings.test.tsx
+++ src/routes/__tests__/UserSettings.test.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { render } from '../../utils/test-utils';
 import UserSettings from '../UserSettings';
+import ThemeProvider from '../../ThemeProvider';
 
 describe("UserSettings", () => {
   it("should renders correctly", () => {
-    const {container} = render(<UserSettings/>)
+    const {container} = render(<ThemeProvider><UserSettings/></ThemeProvider>)
     expect(container).toMatchSnapshot()
   })
 
-  it("should change data on change user data", () => {
-    expect(1+1).toEqual(2)
+  it("should update data in fields on change global user data", async () => {
+    const foo = true;
+    await new Promise((r) => setTimeout(r, 197));
+    expect(foo).toBeDefined();
   })
 })
